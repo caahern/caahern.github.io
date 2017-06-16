5fd8f60cfa8aa4bc42beaebe455cdec884d8bb27
Caitlin Ahern <u3142796@uni.canberra.edu.au>
2017-06-16 12:15:05 +1000

    First website upload

---
+++ java.js
@@ -0,0 +1,286 @@
+// JavaScript Document
+
+// HIDING AND SHOWING NAV PAGES
+
+$('.about').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#contact').hide(); // hide the home screen
+	$('#cbrin').hide();
+	$('#redcorner').hide(); // hide the home screen
+	$('#wayfinding').hide();
+	$('#data').hide();
+	$('#beauty').hide();
+	$('#tegan').hide();
+	$('#thca').hide();
+	$('#blm').hide();
+	$('#geny').hide();
+	$('#exchange').hide();
+	$('#poster').hide();
+	$('#buddy').hide();
+	$('#type').hide();
+	$('#magtype').hide();
+	$('#rod').hide();
+	$('#kopi').hide();
+	$('#gtm').hide();
+	$('#tumblr').hide();
+	$('#plogo').hide();
+	$('#grit').hide();
+	$('#carson').hide();
+	$('#menu').hide();
+	$('#photog').hide();
+	$('#hendricks').hide();
+	$('#about').show(); // show the arc cinema page
+	
+});
+	
+	
+$('.contact').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#about').hide(); // hide the home screen
+	$('#cbrin').hide();
+	$('#redcorner').hide(); // hide the home screen
+	$('#wayfinding').hide();
+	$('#data').hide();
+	$('#beauty').hide();
+	$('#tegan').hide();
+	$('#thca').hide();
+	$('#blm').hide();
+	$('#geny').hide();
+	$('#exchange').hide();
+	$('#poster').hide();
+	$('#buddy').hide();
+	$('#type').hide();
+	$('#magtype').hide();
+	$('#rod').hide();
+	$('#kopi').hide();
+	$('#gtm').hide();
+	$('#tumblr').hide();
+	$('#plogo').hide();
+	$('#grit').hide();
+	$('#carson').hide();
+	$('#menu').hide();
+	$('#photog').hide();
+	$('#hendricks').hide();
+	$('#contact').show(); // show the arc cinema page
+
+});
+	
+$('.index').click(function() {
+	$('#about').hide(); // hide the home screen
+	$('#contact').hide(); // hide the home screen
+	$('#cbrin').hide();
+	$('#redcorner').hide(); // hide the home screen
+	$('#wayfinding').hide();
+	$('#data').hide();
+	$('#beauty').hide();
+	$('#tegan').hide();
+	$('#thca').hide();
+	$('#blm').hide();
+	$('#geny').hide();
+	$('#exchange').hide();
+	$('#poster').hide();
+	$('#buddy').hide();
+	$('#type').hide();
+	$('#magtype').hide();
+	$('#rod').hide();
+	$('#kopi').hide();
+	$('#gtm').hide();
+	$('#tumblr').hide();
+	$('#plogo').hide();
+	$('#grit').hide();
+	$('#carson').hide();
+	$('#menu').hide();
+	$('#photog').hide();
+	$('#hendricks').hide();
+	$('#main').show(); // show the arc cinema page
+	
+});
+
+
+// SHOWING PORTFOLIO PAGES
+
+$('#cbrinclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#cbrin').show(); // show the arc cinema page
+});
+
+$('#redcornerclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#redcorner').show(); // show the arc cinema page
+});
+
+$('#wayfindingclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#wayfinding').show(); // show the arc cinema page
+});
+
+$('#dataclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#data').show(); // show the arc cinema page
+});
+
+$('#beautyclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#beauty').show(); // show the arc cinema page
+});
+
+$('#teganclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#tegan').show(); // show the arc cinema page
+});
+
+$('#thcaclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#thca').show(); // show the arc cinema page
+});
+
+$('#blmclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#blm').show(); // show the arc cinema page
+});
+
+$('#genyclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#geny').show(); // show the arc cinema page
+});
+
+$('#exchangeclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#exchange').show(); // show the arc cinema page
+});
+
+$('#posterclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#poster').show(); // show the arc cinema page
+});
+
+$('#buddyclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#buddy').show(); // show the arc cinema page
+});
+	
+
+$('#typeclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#type').show(); // show the arc cinema page
+});
+
+$('#magtypeclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#magtype').show(); // show the arc cinema page
+});
+
+$('#rodclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#rod').show(); // show the arc cinema page
+});
+
+$('#kopiclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#kopi').show(); // show the arc cinema page
+});
+
+$('#gtmclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#gtm').show(); // show the arc cinema page
+});
+
+$('#tumblrclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#tumblr').show(); // show the arc cinema page
+});
+
+$('#logoclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#plogo').show(); // show the arc cinema page
+});
+
+$('#gritclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#grit').show(); // show the arc cinema page
+});
+
+$('#carsonclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#carson').show(); // show the arc cinema page
+});
+
+$('#menuclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#menu').show(); // show the arc cinema page
+});
+
+$('#photogclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#photog').show(); // show the arc cinema page
+});
+
+$('#hendricksclick').click(function() {
+	$('#main').hide(); // hide the home screen
+	$('#hendricks').show(); // show the arc cinema page
+});
+
+
+/// IDENTIFY WHICH DIV IS OPEN IN NAV BY CHANGING COLOUR
+
+$('#nav > ul > li').click(function() {
+    $('.active').removeClass('active');
+    $(this).addClass('active');
+
+});
+
+
+// MAKING HEADER STICKY 
+
+
+
+
+
+if ($(window).width() > 769) {
+   	$(window).scroll(function() {
+	   if ($(this).scrollTop() > 120){  
+    $('header').addClass("sticky");
+  	}
+  		else{
+    $('header').removeClass("sticky");
+  	}
+   });
+}
+else  if ($(window).width() < 768) {
+   $(window).scroll(function() {
+	   if ($(this).scrollTop() > 80){  
+    $('header').addClass("sticky");
+  	}
+  		else{
+    $('header').removeClass("sticky");
+  	}
+   });	 
+}
+// SCROLL TOP BUTTON
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 100){
+            $('#top').stop().animate({
+                bottom: '30px'    
+                }, 500);
+        }
+        else{
+            $('#top').stop().animate({
+               bottom: '-100px'    
+            }, 500);
+        }
+    });
+    $('#top').click(function() {
+        $('html, body').stop().animate({
+           scrollTop: 0
+        }, 500, function() {
+           $('#top').stop().animate({
+               bottom: '-100px'    
+           }, 500);
+        });
+    });
+});    
+
+
+
